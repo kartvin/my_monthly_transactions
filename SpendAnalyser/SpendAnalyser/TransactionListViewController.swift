159bd42bd90fa456f329791e8beac428b2cfd017
kartvin <karthik.mbe@gmail.com>
2017-01-05 22:21:38 -0500

    Added dummy virtual objects to populate the list

--- SpendAnalyser/SpendAnalyser/TransactionListViewController.swift
+++ SpendAnalyser/SpendAnalyser/TransactionListViewController.swift
@@ -9,29 +9,57 @@
 import UIKit
 
 class TransactionListViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
-    
     @IBOutlet weak var tableView: UITableView!
+    var transactionListDict: NSMutableDictionary?
+    var sectionsList: NSMutableArray?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let arrayList = NSMutableArray()
+        
+        for _ in 1...10 {
+            let transactionVO = TransactionVO()
+            transactionVO.amount = 1321
+            transactionVO.merchant = "Dunkin Donuts"
+            transactionVO.transactionTime = "12/12/2006"
+            
+            arrayList.add(transactionVO);
+        }
+        
+        self.transactionListDict = ["12/12/2006" : arrayList,
+                                    "12/11/2006" : arrayList]
+        
+        self.sectionsList = ["12/12/2006", "12/11/2006"]
+        self.tableView.reloadData()
+    }
     
     // MARK: - Table view data source
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TransactionCellIndentifier", for: indexPath)
-        cell.textLabel?.text = "Testing"
-        cell.detailTextLabel?.text = "Delicious!"
+        let transactions = self.transactionListDict![sectionsList![indexPath.section]] as? [TransactionVO]
+        guard transactions != nil else {
+            return cell
+        }
+        
+        let transaction = transactions?[indexPath.row]
+        cell.textLabel?.text = transaction?.merchant as? String
+        cell.detailTextLabel?.text = String(describing: transaction?.amount)
         return cell
     }
     
     func tableView(_: UITableView, titleForHeaderInSection section: Int) -> String? {
         
-        return "Test"
+        return self.sectionsList?[section] as! String?
         
     }
  
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return self.sectionsList!.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        let list = self.transactionListDict?[sectionsList?[section] ?? []] as! NSMutableArray
+        return list.count
     }
 }
---
+++ SpendAnalyser/SpendAnalyser/TransactionVO.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionVO.swift
+//  SpendAnalyser
+//
+//  Created by Karthik Kumaravel on 1/5/17.
+//  Copyright © 2017 Karthik Kumaravel. All rights reserved.
+//
+
+import Foundation
+
+class TransactionVO : NSObject {
+    //Initialization with default values to avoid NPE
+    var amount :Int = 0
+    var merchant :NSString = ""
+    var transactionTime :NSString = ""
+    
+}
