07a3a9a5856edebe58e89323f0926c2192b180fe
kartvin <karthik.mbe@gmail.com>
2017-01-06 04:40:19 -0500

    aded ui test base

--- SpendAnalyser/SpendAnalyserUITests/SpendAnalyserUITests.swift
+++ SpendAnalyser/SpendAnalyserUITests/SpendAnalyserUITests.swift
@@ -13,14 +13,8 @@ class SpendAnalyserUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
@@ -28,9 +22,20 @@ class SpendAnalyserUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSpendAnalyserListView() {
+        let app = XCUIApplication()
+        sleep(5)
+        let spendAnalyserButton = app.tabBars.buttons["Spend analyser"]
+        XCTAssertTrue(spendAnalyserButton.exists)
+        spendAnalyserButton.tap()
+    }
+    
+    func testTransactionListView() {
+        let app = XCUIApplication()
+        sleep(5)
+        let spendAnalyserButton = app.tabBars.buttons["Transactions"]
+        XCTAssertTrue(spendAnalyserButton.exists)
+        spendAnalyserButton.tap()
     }
     
 }
